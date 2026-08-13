d747938bb2f0a237055d1d9cd35113c7c3561109
Amit pant <139322726+pantamit2003@users.noreply.github.com>
2026-08-13 11:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-SHEET_ID = "1iWANnFQYSygr-UnsguJFnmFRf4sJ-UceyrC6x1hC-lE"
+SHEET_ID = "1HAolJrhXfoeYTL7lYCGsHuD9o1ljmfU8Q6ELhsvGNDM"
 
 CREDENTIALS_FILE = BASE_DIR / "credentials.json"
 
